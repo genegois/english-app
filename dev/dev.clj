977fe7713b13971dacdc739acf6636f0ca28a24d
Erlangga <92940700+genegois@users.noreply.github.com>
2025-09-28 21:35:35 +0700

    rapiin dev.clj

--- dev/dev.clj
+++ dev/dev.clj
@@ -54,47 +54,9 @@
   (stop)
 
 
-  (-> @dev-system :openai)
-  (def aid "f9d7bda0989511f095f87e6d5a3d47f1")
-  (def dummy-answers
-    (mapv (fn [i] {:selected (rand-int 4)}) (range 15)))
-  
-  (app.backend.features.english/submit-assessment!
-   (-> @dev-system :dbase :db) (-> @dev-system :openai)
-   aid
-   dummy-answers)
-  
-  (map (fn [q ans]
-         (let [correct (:answer-idx q)
-               chosen (:selected ans)
-               ok? (= correct chosen)]
-           (assoc q :user-answer chosen :correct? ok?)))
-       (:questions (mc/find-one-as-map (-> @dev-system :dbase :db) "assessments" {:_id aid}))
-       dummy-answers)
-  
-  (:questions (mc/find-one-as-map (-> @dev-system :dbase :db) "assessments" {:_id aid}))
-
   (app.backend.features.english/fetch-prosets (-> @dev-system :dbase :db) "98660320988511f0adef7e6d5a3d47f1")
   (app.backend.features.english/generate-assessment!  (-> @dev-system :dbase :db) (-> @dev-system :openai) "441bd870989511f095f87e6d5a3d47f1" )
   
-  
-  
-
-
-
   ;; if db is not seeded with user, it needs to do this
   (app.plumbing.db/init-admin (:dbase @dev-system) "[insert password]")
-  (app.plumbing.db/init-trash (:dbase @dev-system))
-  ;; note, [mbe todo], make an interface to run this later on
-  
-  ;; mbe basic test note may 10th 2023
-  ;;  - [minor - todo - any] admin change role, no notif if the save action succeed
-  ;;   - /dashboard/UserManagement
-  ;;
-  ;;  - [major - todo - any] connecting to content no content-id validation
-  ;;   - /dashboard/SkillManagement
-  ;;
-  ;;  - [major - todo - any] no content id preview
-  ;;   -  /dashboard/ContentManagement
-  ;;
-  )
+  (app.plumbing.db/init-trash (:dbase @dev-system)))
